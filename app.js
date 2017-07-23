01ba08a1e5e9b9aa1a82b50595db8ab9e3424efa
Ilamaran Gurunathan <ilamaran.g@gmail.com>
2017-07-23 07:04:35 -0400

    Commit before Heroku push

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.use('/services', services);
 app.use('/submitPoll', poll);
 
 // Set Port
-db.sequelize.sync({force:false}).then(function() {
+db.sequelize.sync({force:true}).then(function() {
   app.listen(3000, function() {
     console.log("port listening on " + 3000);
   });
